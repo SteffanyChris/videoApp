637c9ecf52fd83b8afde548d2b5b6d3672dc7286
Stefi <cialmastefana@gmail.com>
2021-05-09 18:28:16 +0300

    some more eslint..

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,17 +1,17 @@
-import { all, call } from 'redux-saga/effects';
+import {all, call} from 'redux-saga/effects';
 import moviesSagas from './movies/sagas';
 import tvSagas from './tv/sagas';
 import genreSagas from './assetsByGenre/sagas';
 import searchSagas from './search/sagas';
 
 /* In case the application gets more complex and we have more
-*   features, we can add more sagas to the array 
-*/
+ *   features, we can add more sagas to the array
+ */
 export default function* rootSaga() {
-    yield all([
-        call(moviesSagas),
-        call(tvSagas),
-        call(genreSagas),
-        call(searchSagas)
-    ])
+  yield all([
+    call(moviesSagas),
+    call(tvSagas),
+    call(genreSagas),
+    call(searchSagas),
+  ]);
 }
--- src/redux/tv/sagas.js
+++ src/redux/tv/sagas.js
@@ -1,36 +1,41 @@
-import { takeLatest, put, call, all, takeEvery } from 'redux-saga/effects';
-import { fetchMoreTvFailed, fetchMoreTvSuccess, fetchPopularShowsFailed, fetchPopularShowsSuccess} from './actions';
+import {takeLatest, put, call, all, takeEvery} from 'redux-saga/effects';
+import {
+  fetchMoreTvFailed,
+  fetchMoreTvSuccess,
+  fetchPopularShowsFailed,
+  fetchPopularShowsSuccess,
+} from './actions';
 import actionTypes from './actionTypes';
-import { getMoreTvShows, getPopularTvShows } from '../../utils/axiosUtils';
+import {getMoreTvShows, getPopularTvShows} from '../../utils/axiosUtils';
 
-function* fetchPopularShowsAsync(){
-	try{
-		const popularShows = yield getPopularTvShows()
-		yield put(fetchPopularShowsSuccess(popularShows))
-	}catch(error){
-		yield put(fetchPopularShowsFailed(error));
-	}
+function* fetchPopularShowsAsync() {
+  try {
+    const popularShows = yield getPopularTvShows();
+    yield put(fetchPopularShowsSuccess(popularShows));
+  } catch (error) {
+    yield put(fetchPopularShowsFailed(error));
+  }
 }
 
-function* fetchMoreTvAsync({payload}){
-	try{
-		const moreShows = yield getMoreTvShows(payload)
-		yield put(fetchMoreTvSuccess(moreShows))
-	}catch(error){
-		yield put(fetchMoreTvFailed(error));
-	}
+function* fetchMoreTvAsync({payload}) {
+  try {
+    const moreShows = yield getMoreTvShows(payload);
+    yield put(fetchMoreTvSuccess(moreShows));
+  } catch (error) {
+    yield put(fetchMoreTvFailed(error));
+  }
 }
 
 function* fetchPopularShowsStart() {
-	yield takeLatest(actionTypes.FETCH_POPULAR_SHOWS_START, fetchPopularShowsAsync)
+  yield takeLatest(
+    actionTypes.FETCH_POPULAR_SHOWS_START,
+    fetchPopularShowsAsync,
+  );
 }
-function* fetchMoreTvStart(){
-	yield takeEvery(actionTypes.FETCH_MORE_TV_START, fetchMoreTvAsync)
+function* fetchMoreTvStart() {
+  yield takeEvery(actionTypes.FETCH_MORE_TV_START, fetchMoreTvAsync);
 }
 
-export default function* tvSagas(){
-	yield all([
-		call(fetchPopularShowsStart),
-		call(fetchMoreTvStart)
-	])
+export default function* tvSagas() {
+  yield all([call(fetchPopularShowsStart), call(fetchMoreTvStart)]);
 }
